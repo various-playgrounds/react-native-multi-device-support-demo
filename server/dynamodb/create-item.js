c1dca3cccd5793f0af709146e9028252fc23a7b7
guanw <guanw0826@gmail.com>
2020-04-27 19:53:34 -0400

    update create-item

--- server/dynamodb/create-item.js
+++ server/dynamodb/create-item.js
@@ -1,9 +1,48 @@
+// var params = {
+//     TableName: 'ct152',
+//     Item: {
+//         pk: 'userId#1',
+//         exercises: {},
+//         version: 0
+//     },
+//     ReturnValues: 'NONE',
+//     ReturnConsumedCapacity: 'NONE'
+// }
+// docClient.put(params, function(err, data) {
+//     if (err) ppJson(err);
+//     else ppJson(data);
+// });
+
+
+
 var params = {
     TableName: 'ct152',
     Item: {
         pk: 'userId#1',
         exercises: {},
-        version: 0
+        versions: {
+            exercises: {
+                "0": 0,
+                "1": 0,
+                "2": 0,
+                "3": 0,
+                "4": 0,
+                "5": 0,
+                "6": 0,
+                "7": 0,
+                "8": 0,
+                "9": 0,
+                "10": 0,
+                "11": 0,
+                "12": 0,
+                "13": 0,
+                "14": 0,
+                "15": 0,
+                "16": 0,
+                "17": 0,
+                "18": 0
+            }
+        }
     },
     ReturnValues: 'NONE',
     ReturnConsumedCapacity: 'NONE'
